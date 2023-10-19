4218d747d5f41ef1fb3fc83bcc7d3bd9a227f3ee
Ayush Thapa <thapaaayush115@gmail.com>
2023-10-19 09:39:00 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# PickyFlats - Flat/Apartments Rental web app
+
+- PickyFlats is a web-based rental platform connecting renters with flats and apartments.
+
+## Tech Stacks
+
+- ⚡️ Next.js 13
+- ⚛️ React 18
+- ✨ TypeScript
+- 💨 Tailwind CSS / MUI - (Material UI)
+- ⚡️ Appwrite Cloud
---
+++ src/features/chat/ChatMessages.tsx
@@ -0,0 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+
+import {
+  client,
+  CONVERSATIONS_ID,
+  DATABASE_ID,
+  MESSAGES_BUCKET,
+  storage,
+} from '@/lib/client';
+
+import { getMessageById } from '@/database/message';
+
+import MessageItem from '@/components/chat/MessageItem';
+
+import useChatStore from '@/store/useChatStore';
+import useLightBoxStore from '@/store/useLightBoxStore';
+
+import { UserProfile } from '@/types/user';
+
+export default function ChatMessages({
+  conversationId,
+  chatUser,
+  cbOnNewMessage,
+}: {
+  conversationId;
+  chatUser?: UserProfile;
+  cbOnNewMessage?: () => void;
+}) {
+  const { messages, onNewMessage } = useChatStore();
+  const { setImages } = useLightBoxStore();
+  React.useEffect(() => {
+    const unsubscribe = client.subscribe(
+      `databases.${DATABASE_ID}.collections.${CONVERSATIONS_ID}.documents.${conversationId}`,
+      (chat) => {
+        const chatPayload: any = chat.payload;
+        const newMessageId = chatPayload?.lastMessageID;
+
+        const loadNewMessage = async () => {
+          const newMessage = await getMessageById(newMessageId);
+          onNewMessage(newMessage);
+
+          //TODO: ! same msg push render issue
+        };
+        loadNewMessage();
+        // cb for on new message
+        cbOnNewMessage?.();
+      }
+    );
+
+    return () => unsubscribe();
+  }, [cbOnNewMessage, conversationId, onNewMessage]);
+
+  React.useEffect(() => {
+    const _attachments: string[] = [];
+    messages.allIds
+      .filter((id) => messages.byId[id]?.attachments.length > 0)
+      .map((id) => {
+        const __attachments: string[] = [];
+        messages.byId[id].attachments.map((a) => {
+          const attachment = storage.getFileView(MESSAGES_BUCKET, a);
+          __attachments.push(attachment.href);
+        });
+        _attachments.push(...__attachments);
+      });
+    setImages(_attachments);
+  }, [messages]);
+
+  return (
+    <>
+      <div className='flex flex-col space-y-4'>
+        {messages.allIds.map((messageID, index) => {
+          const message = messages.byId[messageID];
+          if (!message) return <></>;
+          return (
+            <MessageItem
+              key={index}
+              index={index}
+              message={message}
+              chatUser={chatUser}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
